07ff94e22b0a1e94cb4635813ff617773b554ee0
tuplesxyz <tuplesxyz@protonmail.com>
2020-11-10 10:31:08 +0000

    set up basic server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+
+const port = process.env.PORT || 1337
+const app = express();
+
+app.get('/products', listProducts)
+app.listen(port,  () => console.log(`Server listening on port ${port}`))
+
+async function listProducts(req, res) {
+    const productsFile = path.join(__dirname, 'products.json')
+    try {
+        const data = await fs.readFile(productsFile)
+        res.json(JSON.parse(data)) }
+        catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+}
+
