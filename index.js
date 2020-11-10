9326dbf4180e19b4a031466cc0de91c402d2c461
tuplesxyz <tuplesxyz@protonmail.com>
2020-11-10 12:47:35 +0000

    add middleware to handle notfound and errors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { listProducts, getProduct } = require('./api');
-const { cors } = require('./middleware');
+const { cors, notFound, handleError } = require('./middleware');
 const port = process.env.PORT || 1337;
 const app = express();
 
@@ -8,6 +8,9 @@ app.use(cors);
 
 app.get('/products', listProducts);
 app.get('/products/:id', getProduct);
+
+app.use(handleError)
+app.use(notFound)
 
 app.listen(port,  () =>
     console.log(`Server listening on port ${port}`)
--- middleware.js
+++ middleware.js
@@ -26,6 +26,18 @@ function cors (req, res, next) {
     next()
 }
 
+function handleError (err, req, res, next) {
+    console.error(err)
+    if (res.headersSent) return next(err)
+    res.status(500).json({ error: 'Internal Error' })
+}
+
+function notFound (req, res) {
+    res.status(404).json({ error: 'Not Found' })
+}
+
 module.exports = {
-    cors
+    cors,
+    handleError,
+    notFound
 }
